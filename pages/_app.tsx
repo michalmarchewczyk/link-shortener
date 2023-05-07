6839cfd7035c7a7dc27e644be67187d239c58ded
Michał Marchewczyk <dev@marchewczyk.eu>
2023-05-08 01:41:13 +0200

    Setup Mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { emotionCache } from '@/lib/emotion-cache';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS withCSSVariables emotionCache={emotionCache}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { createStylesServer, ServerStyles } from '@mantine/next';
+import { emotionCache } from '@/lib/emotion-cache';
 
-export default function Document() {
+export default function _Document() {
   return (
     <Html lang="en">
       <Head />
@@ -11,3 +13,14 @@ export default function Document() {
     </Html>
   );
 }
+
+const stylesServer = createStylesServer(emotionCache);
+
+_Document.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [initialProps.styles, <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />],
+  };
+};
