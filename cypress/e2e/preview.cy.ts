dadce30c16d7e66414dc31a56e7523388c512939
Michał Marchewczyk <dev@marchewczyk.eu>
2023-05-18 15:49:08 +0200

    Add e2e tests for previewing links

---
+++ cypress/e2e/preview.cy.ts
@@ -0,0 +1,36 @@
+describe('Preview', () => {
+  it('automatically preview shortened link', () => {
+    cy.visit('/');
+    cy.get('input[placeholder="Your URL"]').type('https://example.com');
+    cy.contains('button', 'Shorten').click();
+
+    cy.location('pathname').should('include', '/view/');
+    cy.contains('div', 'Shortened').should('include.text', 'URL:https://example.com');
+    cy.contains('div', 'Shortened').should('include.text', 'Slug:');
+    cy.contains('div', 'Shortened').should('include.text', 'Views:0');
+    cy.contains('div', 'Shortened').should('include.text', 'Created:');
+    cy.contains('div', 'Shortened').should('include.text', 'Last update:');
+
+    cy.contains('button', 'Go back').click();
+    cy.location('pathname').should('equal', '/view');
+  });
+
+  it('preview link by slug', () => {
+    cy.visit('/');
+    cy.get('input[placeholder="Your URL"]').type('https://example.com');
+    cy.get('input[placeholder^="Custom slug"]').type('test-preview-slug');
+    cy.contains('button', 'Shorten').click();
+
+    cy.visit('/view');
+    cy.get('input[placeholder="Slug"]').type('test-preview-slug');
+    cy.contains('button', 'View').click();
+
+    cy.location('pathname').should('include', '/view/test-preview-slug');
+    cy.contains('div', 'Shortened').should('include.text', 'URL:https://example.com');
+    cy.contains('div', 'Shortened').should('include.text', 'Slug:test-preview-slug');
+    cy.contains('div', 'Shortened').should('include.text', 'Views:0');
+
+    cy.contains('button', 'Go back').click();
+    cy.location('pathname').should('equal', '/view');
+  });
+});
